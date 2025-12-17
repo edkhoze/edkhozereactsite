1eb8e52e76df30f132c90c5693fa530802abb069
edkhoze <personal@edkhoze.com>
2025-12-17 18:38:58 +1100

    adding Chatbase chatbox widget onto the site

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface Window {
+  chatbase: {
+    (command: string, ...args: any[]): void;
+    (command: 'identify', data: { token: string }): void;
+    (command: 'getState'): string;
+    q?: any[];
+  };
+}
